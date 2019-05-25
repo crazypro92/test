9bd5c82bba06420b61101b43c07a8c933c9640f0
crazypro92 <crazypro92@ukr.net>
2019-05-25 12:16:12 +0300

    Выполнили первое задание

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 var number = 5;
 var string = "Hello";
 var sym = Symbol();
@@ -45,4 +45,30 @@ console.log("2" === 2);
 let isCheked = true,
     isClose = false;
 
-console.log(isCheked || isClose);    
+console.log(isCheked || isClose); 
+
+*/
+
+// Zadanie
+
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
