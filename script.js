badae65c8eb59c42afcf0fc1863cb93e4daa8870
crazypro92 <crazypro92@ukr.net>
2019-06-08 15:45:51 +0300

    Выполнил задание 8

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Lessons 20
+/*
 let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
@@ -27,4 +29,41 @@ wrapper.removeChild(heart[2]); // Удаляем єлемент
 document.body.replaceChild(btn[1], circle[1]); //Замена одного элемента на другой
 //div.innerHTML = '<h2>Hello World</h2>'; // Добавление текста или элемента в элемент - 1
 div.textContent = 'Hello World'; // Добавление текста или элемента в элемент - 2
+*/
+//Lessons 21
 
+let box = document.getElementById('box');
+
+// box.onclick = function() {
+//     alert('Мы кликнули на первый бокс');
+// }; // При клике выводит сообщение
+
+// box.addEventListener('click', function() {
+//     alert('Мы кликнули на первый бокс');
+//     alert('Мы кликнули на первый бокс повторно');
+// });   // При клике операции выполняются поочерёдно
+
+// box.addEventListener('mouseenter', function() {
+//     alert('Мы навели мышь на первый бокс');  
+//});   // При наведении на элемент
+box.addEventListener('click', function(e) {
+    let target = e.target; // присвоение нашему элементу переменую
+    target.style.display = 'none'; //делаем блок невидимым
+    console.log('Произошло событие ' + e.type + ' на элементе ' + e.target);
+    //Произошло событие click на элементе [object HTMLDivElement]
+})
+
+let link = document.querySelector('a');
+
+link.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('Произошло событие ' + e.type + ' на элементе ' + e.target);
+});
+
+let btn = document.querySelectorAll('button');
+
+btn.forEach( function(item) {
+    item.addEventListener('mouseleave', function(e){
+        console.log('Мы убрали мышку с кнопок');
+    })
+}) // Функция когда нам надо работать с массивос обьектов
